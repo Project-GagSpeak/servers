4f7bf841b9b41232d02d9dacac70fd83b78d3517
Cordelia Mist <buzzmodetrix@gmail.com>
2024-07-26 12:30:22 -0700

    new db update

---
+++ GagSpeakServerCollection/GagSpeakShared/Migrations/20240726192814_2024-07-26-RestructuredPrivateRoomModels.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GagSpeakShared.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20240726RestructuredPrivateRoomModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "allowing_vibe",
+                table: "private_room_users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "in_room",
+                table: "private_room_users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "allowing_vibe",
+                table: "private_room_users");
+
+            migrationBuilder.DropColumn(
+                name: "in_room",
+                table: "private_room_users");
+        }
+    }
+}
