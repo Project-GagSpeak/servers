1e22126cb338174da96d16a9cc8fa27fbf771d8a
Cordelia Mist <buzzmodetrix@gmail.com>
2024-11-03 15:41:37 -0800

    DB update

--- GagSpeakServerCollection/GagSpeakShared/Migrations/20241103234048_2024-11-03NewProfileContent2.Designer.cs
+++ GagSpeakServerCollection/GagSpeakShared/Migrations/20241103234048_2024-11-03NewProfileContent2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GagspeakShared.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GagSpeakShared.Migrations
 {
     [DbContext(typeof(GagspeakDbContext))]
-    partial class GagspeakDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241103234048_2024-11-03NewProfileContent2")]
+    partial class _20241103NewProfileContent2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GagSpeakServerCollection/GagSpeakShared/Migrations/20241103234048_2024-11-03NewProfileContent2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GagSpeakShared.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20241103NewProfileContent2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "completed_achievements_total",
+                table: "user_profile_data",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "completed_achievements_total",
+                table: "user_profile_data");
+        }
+    }
+}
