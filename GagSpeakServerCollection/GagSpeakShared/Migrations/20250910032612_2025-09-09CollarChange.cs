d244ec9457d9d571b5a37242836a5cf52d2660d3
Cordelia Mist <buzzmodetrix@gmail.com>
2025-09-09 20:27:35 -0700

    db changes.

---
+++ GagSpeakServerCollection/GagSpeakShared/Migrations/20250910032612_2025-09-09CollarChange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GagSpeakShared.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20250909CollarChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "identifier",
+                table: "user_collar_data");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "identifier",
+                table: "user_collar_data",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
